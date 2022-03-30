8769f2c3e310b3480aaeab2eb7f579cf5ccfdab1
卫正明 <1904628089@qq.com>
2022-03-31 00:24:52 +0800

    数æ学习处理复杂数据类型

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -8,7 +8,10 @@ class Users {
     }
 
     async findById(ctx) {
-        const user =  await User.findById(ctx.params.id)
+        // 获取字段
+        const {fields} = ctx.query;
+        const selectFields =fields.split(';').filter(f=>f).map(f=>' +' +f).join('')
+        const user =  await User.findById(ctx.params.id).select(selectFields)
         if(!user){
             ctx.throw(404,'用户未找到')
         }
@@ -35,7 +38,13 @@ class Users {
     async update(ctx) {
         ctx.verifyParams({
             name: {type: 'string', required: false},
-            password: {type: 'string', required: false}
+            password: {type: 'string', required: false},
+            avatar_url: {type: 'string', required: false},
+            gender: {type: 'string', required: false},
+            headLine: {type: 'string', required: false},
+            location: {type: 'array', itemType:'string',required: false},
+            employments: {type: 'array', itemType:'object',required: false},
+            educations: {type: 'array', itemType:'object',required: false},
         })
         const user = await User.findByIdAndUpdate(ctx.params.id,ctx.request.body)
         if(!user){
--- app/models/users.js
+++ app/models/users.js
@@ -5,6 +5,36 @@ const {Schema,model} = mongoose
 const userSchema =  new Schema({
     __v:{type:Number,select:false},
     name:{type:String, required:true},
-    password:{type:String,required:true,select:false}
+    password:{type:String,required:true,select:false},
+    //头像
+    avatar_url:{type:String},
+    //性别，枚举
+    gender:{type:String,enum:['male','female'],default:'male'},
+    // 一句话介绍自己
+    headLine:{type:String},
+    // 地址,数组
+    location:{type:[{type:String}],select:false },
+    // 工作经历
+    employments:{
+        type:[
+            {
+                company:{type:String},
+                job:{type:String},
+            }
+        ],select:false
+
+    },
+    // 教育经历
+    educations:{
+        type:[
+            {
+                school:{type:String},
+                major:{type:String},
+                diploma:{type:Number,enum:[1,2,3,4,5]},
+                entrance_year:{type:Number},
+                graduate_year:{type:Number}
+            }
+        ],select:false
+    }
 })
 module.exports = model('User',userSchema)
